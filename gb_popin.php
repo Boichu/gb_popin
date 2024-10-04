347fd6f7d8a6c2b20736135a31d3eb275e83706c
Gaetan_Boishue <gaetan@wwwup.fr>
2024-10-04 11:58:26 +0200

    une maj pour teste la maj

--- gb_popin.php
+++ gb_popin.php
@@ -3,7 +3,7 @@
 Plugin Name: GB Popin
 Plugin URI: https://github.com/Boichu/gb_popin
 Description: Gestion d'une popin de bienvenue légère avec une image en paysage et une autre en portrait, un lien de redirection, un temps d'affichage, un temps de non-affichage si le client ferme la popin et un temps de non-affichage si le client passe commande.
-Version: 1.0.0
+Version: 1.0.2
 Author: Votre Nom
 Author URI: https://votre-site.com
 License: GPL2
