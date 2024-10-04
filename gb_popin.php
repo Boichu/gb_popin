12e82f19664d0b9ff8f1b742d063f30af4589ccf
Gaetan_Boishue <gaetan@wwwup.fr>
2024-10-04 13:22:35 +0200

    maj description

--- gb_popin.php
+++ gb_popin.php
@@ -4,8 +4,8 @@ Plugin Name: GB Popin
 Plugin URI: https://github.com/Boichu/gb_popin
 Description: Gestion d'une popin de bienvenue légère avec une image en paysage et une autre en portrait, un lien de redirection, un temps d'affichage, un temps de non-affichage si le client ferme la popin et un temps de non-affichage si le client passe commande.
 Version: 1.0.2
-Author: Votre Nom
-Author URI: https://votre-site.com
+Author: Gaétan Boishue
+Author URI: https://www.pagespeedlab.com/
 License: GPL2
 GitHub Plugin URI: https://github.com/Boichu/gb_popin
 GitHub Branch: main
